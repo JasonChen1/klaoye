ffd837e03b41e8013c715e33a96f228c5a65346d
jason <difuchen@gmail.com>
2021-08-05 17:44:46 +0800

    add google fb pixel

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -18,3 +18,30 @@
 <script src="https://js.stripe.com/v3/"></script>
 <script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
 <script src="https://www.paypal.com/sdk/js?client-id=AWSmixG4AV1q_j_zHLasTbAoHpncIBwnBWu0H2-fZ-cJvwTG2_R1gnWgqufBMQYPkrDWV3SfU--_79kE&currency=USD" data-sdk-integration-source="button-factory"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-77DV71KBE6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-77DV71KBE6');
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '2960353174178853');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=2960353174178853&ev=PageView&noscript=1"/></noscript>
+<!-- End Facebook Pixel Code -->
